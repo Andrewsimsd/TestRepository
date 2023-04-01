2e27d8249552d29bfa4972ffa01c7f2c4e1bd1a7
chrisdbeard <88802276+chrisdbeard@users.noreply.github.com>
2023-04-01 15:48:27 -0400

    Update Form1.cs
    Changed message.

--- TestSolution/TestProject/Form1.cs
+++ TestSolution/TestProject/Form1.cs
@@ -19,7 +19,7 @@ namespace TestProject
 
         public void SendMessage()
         {
-            Console.WriteLine("A Message");
+            Console.WriteLine("New Message.");
         }
     }
 }
